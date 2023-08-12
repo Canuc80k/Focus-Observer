71eac4773ec963eea2df8cf9d792221ec1f6b30e
Canuc80k <Canuc80k@gmail.com>
2023-08-12 15:03:54 +0700

    Add UI for extension (kinda perfect + chef kiss)

---
+++ popup/popup.js
@@ -0,0 +1,25 @@
+let currID = 0;
+
+let createConsoleLineElement = (id) => {
+    let result = "<div class=\"console-line\">";
+    result += "<div class=\"console-begin-char\">&gt;</div>"
+    result += "<input id=\"" + id + "\" class=\"console-text\" type=\"text\"></input>";
+    result += "</div>"
+
+    return result;
+}
+
+window.onload = (() => {
+    $("#console").append(createConsoleLineElement(currID));
+    $("#" + currID).focus();
+});
+
+$(document).on('keypress', (e) => {
+    if(e.which == 13) {
+        $("#" + currID).blur();
+        $("#console").append(createConsoleLineElement(++ currID));
+        $("#" + currID).focus();
+
+        console.log($('body').height());
+    }
+});
