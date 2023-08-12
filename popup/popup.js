fe98b30c33a972905c5a6984f9e0ace9fd798b38
Canuc80k <Canuc80k@gmail.com>
2023-08-12 18:16:12 +0700

    Update UI

--- popup/popup.js
+++ popup/popup.js
@@ -2,7 +2,7 @@ let currID = 0;
 
 let createConsoleLineElement = (id) => {
     let result = "<div class=\"console-line\">";
-    result += "<div class=\"console-begin-char\">&gt;</div>"
+    result += "<div class=\"console-begin-char\">&gt; </div>"
     result += "<input id=\"" + id + "\" class=\"console-text\" type=\"text\"></input>";
     result += "</div>"
 
