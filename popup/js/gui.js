eee7e8d8d5c77cddaf79a47e1191714f54e6f908
Canuc80k <Canuc80k@gmail.com>
2023-08-16 22:10:32 +0700

    Save current line text for next time

--- popup/js/gui.js
+++ popup/js/gui.js
@@ -15,74 +15,83 @@ const createRequestLine = (id) => {
 }
 
 const createRespondLine = (id) => {
-    return "<div class=\"console-line\"><textarea id=\"r" + id + "\" class=\"respond-console-text\">respond :D" + id + "</textarea></div>";
+    return "<div class=\"console-line\"><textarea id=\"r" + id + "\" class=\"respond-console-text\">respond :D " + id + "</textarea></div>";
 }
 
 const archiveCurrentLine = () => {
     let currID = requestList.length;
-    let requestLine = $("#" + currID);
+    let $requestLine = $("#" + currID);
     
-    requestLine.blur();
-    requestLine.prop('readonly', true);
-    requestList.push(requestLine.val());
-    requestLinesHeight.push(countTextareaLine(requestLine.get(0)) * fontSize);
+    $requestLine.blur();
+    $requestLine.prop('readonly', true);
+    requestList.push($requestLine.val());
+    requestLinesHeight.push(countTextareaLine($requestLine.get(0)) * fontSize);
 
     let currRID = respondList.length;
-    let respondLine = $("#r" + currRID);
-    respondList.push(respondLine.val());
-    respondLinesHeight.push(countTextareaLine(respondLine.get(0)) * fontSize);
+    let $respondLine = $("#r" + currRID);
+    respondList.push($respondLine.val());
+    respondLinesHeight.push(countTextareaLine($respondLine.get(0)) * fontSize);
 
     chrome.storage.sync.set({
         "requestList": requestList, 
         "respondList": respondList,
         "requestLinesHeight": requestLinesHeight,
         "respondLinesHeight": respondLinesHeight,
+        "currentRequestText": ""
     }, () => {});
 }
 
 const addOldRequestLine = (id, text, height) => {
     $console.append(createRequestLine(id));
-    let resquestLine = $("#" + id);
-    resquestLine.prop('readonly', true);
-    resquestLine.text(text);
-    resquestLine.css("height", height + "rem");
+    let $resquestLine = $("#" + id);
+    $resquestLine.prop('readonly', true);
+    $resquestLine.text(text);
+    $resquestLine.css("height", height + "rem");
 }
 
 const addOldRespondLine = (id, text, height) => {
     $console.append(createRespondLine(id));
-    let respondLine = $("#r" + id);
-    respondLine.text(text);
-    respondLine.css("height", height + "rem");
+    let $respondLine = $("#r" + id);
+    $respondLine.text(text);
+    $respondLine.css("height", height + "rem");
 }
 
 const addNewRequestLine = () => {
     let newID = requestList.length;
     $console.append(createRequestLine(newID));
-    let requestLine = $("#" + newID);
+    let $requestLine = $("#" + newID);
     
-    setTimeout(() => {requestLine.focus();});
-    requestLine.bind('input propertychange', (e) => {
-        requestLine.css("height", countTextareaLine(e.target) * fontSize + "rem");
+    setTimeout(() => {$requestLine.focus();});
+    $requestLine.bind('input propertychange', (e) => {
+        $requestLine.css("height", countTextareaLine(e.target) * fontSize + "rem");
+       
+        currentRequestText = $requestLine.val();
+        console.log(currentRequestText);    
+        chrome.storage.sync.set({"currentRequestText": currentRequestText}, () => {});
     });
 }
 
 const addNewRespondLine = () => {
     let newID = respondList.length;
     $console.append(createRespondLine(newID));
-    let respondLine = $("#r" + newID);
-    respondLine.prop('readonly', true);
+    let $respondLine = $("#r" + newID);
+    $respondLine.prop('readonly', true);
 
-    respondLine.bind('input propertychange', (e) => {
-        respondLine.css("height", countTextareaLine(e.target) * fontSize + "rem");
-    });
+    $respondLine.css("height", countTextareaLine($respondLine.get(0)) * fontSize + "rem");
 }
 
-const addCurrentRequestLine = () => {
+const addCurrentRequestLine = (text) => {
     $console.append(createRequestLine(requestList.length));
-    let currInputObject = $("#" + requestList.length);
-    setTimeout(() => {currInputObject.focus();});
-    currInputObject.bind('input propertychange', (e) => {
-        currInputObject.css("height", countTextareaLine(e.target) * fontSize + "rem");
+    let $currRequestLine = $("#" + requestList.length);
+    setTimeout(() => {$currRequestLine.focus();});
+    $currRequestLine.val(text); 
+
+    $currRequestLine.bind('input propertychange', (e) => {
+        currentRequestText = $currRequestLine.val();
+        console.log(currentRequestText);    
+        chrome.storage.sync.set({"currentRequestText": currentRequestText}, () => {});
+
+        $currRequestLine.css("height", countTextareaLine(e.target) * fontSize + "rem");
     });
 }
 
--- popup/js/init.js
+++ popup/js/init.js
@@ -1,6 +1,9 @@
 let requestList, respondList;
 let requestLinesHeight, respondLinesHeight;
+let currentRequestText;
 let $console;
+
+console.log("asd".constructor.name);
 
 const checkDataAvailable = (data, dataType) => {
     if (data != undefined && data.constructor.name == dataType) return true;
@@ -10,18 +13,22 @@ const checkDataAvailable = (data, dataType) => {
 window.onload = () => {
     $console = $("#console");
     chrome.storage.sync.get().then((data) => {
+        currentRequestText = "";
         requestList = [], respondList = [], requestLinesHeight = [], respondLinesHeight = [];
+        console.log(data.currentRequestText);
         if (checkDataAvailable(data.requestList, "Array")) requestList = data.requestList;
         if (checkDataAvailable(data.respondList, "Array")) respondList = data.respondList;
         if (checkDataAvailable(data.requestLinesHeight, "Array")) requestLinesHeight = data.requestLinesHeight;
         if (checkDataAvailable(data.respondLinesHeight, "Array")) respondLinesHeight = data.respondLinesHeight;
-        
+        if (checkDataAvailable(data.currentRequestText, "String")) currentRequestText = data.currentRequestText; 
+
         console.log(requestList);
         console.log(respondList);
         console.log(requestLinesHeight);
         console.log(respondLinesHeight);
-        
+        console.log(currentRequestText);
+
         if (requestList.length) buildOldGUI();
-        addCurrentRequestLine();
+        addCurrentRequestLine(currentRequestText);
     });
 };
