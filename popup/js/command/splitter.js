c8aeb0779b68de94e4b1e08b15bb49b988d88b8b
Canuc80k <Canuc80k@gmail.com>
2023-08-19 21:29:17 +0700

    Split better

--- popup/js/command/splitter.js
+++ popup/js/command/splitter.js
@@ -1,13 +1,23 @@
 const handleCommand = () => {
     let command = currentRequestText.trim().split(" ");
     
-    if (command[0] == "clear" && command.length == 1) {clear(); return;}
-    if (command[0] == "autoclear" && command.length == 1) {showAutoClearCommand(); return;}
-    if (command[0] == "autoclear" && command.length == 2 && command[1] == "-s") {showAutoClear(); return;}
-    if (command[0] == "autoclear" && command.length == 2 && command[1] == "-show") {showAutoClear(); return;}
-    if (command[0] == "autoclear" && command.length == 2 && command[1] == "-d") {autoClear(Number.MAX_SAFE_INTEGER); return;}
-    if (command[0] == "autoclear" && command.length == 2 && command[1] == "-default") {autoClear(Number.MAX_SAFE_INTEGER); return;}
-    if (command[0] == "autoclear" && command.length == 2) {autoClear(command[1]); return;}
+    if (command[0] == "clear" && command.length == 1) {
+        clear(); return;
+    }
+    if (command[0] == "autoclear") {
+        if (command.length == 1) {
+            showAutoClearCommand(); 
+        }
+        else if (command.length == 2) {
+            if (command[1] == "-s" || command[1] == "-show") showAutoClear(); 
+            else if (command[1] == "-d" || command[1] == "-default") autoClear(Number.MAX_SAFE_INTEGER); 
+            else autoClear(command[1]); 
+            return;
+        }
+
+        addNewRespondLine("autoclear arguments aren't available");
+        return;
+    }
 
     addNewRespondLine("Guess nothing to do here...");
 }
